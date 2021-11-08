b07bd9a7011912aef6d14ec9d2e8429230633490
Natkeeran <natkeeran@gmail.com>
2021-11-08 10:21:55 -0500

    editable transcription

--- js/move_content_below.js
+++ js/move_content_below.js
@@ -1,4 +1,9 @@
 jQuery( document ).ready(function() {
   var element = jQuery('.node__content').first().detach();
   jQuery('.region-content').first().append(element);
+
+  var related_doc_nid = jQuery(".field--name-field-related-document").first().children("div").eq(1).find("a").attr('href');
+  var related_doc_manifest = window.location.origin + related_doc_nid.trim() + "/" + "book-manifest";
+  jQuery("#iiif-manifest").attr("href", related_doc_manifest);
+  
 });
